a2a3249c5ab306e9c6a51bcc2694f539eac8fa7d
macasillas <miguel@miguelcasillas.com>
2015-11-28 00:18:32 -0600

    Adding tree borders to levels

--- GameJamGame/Assets/Scripts/Enemy.cs
+++ GameJamGame/Assets/Scripts/Enemy.cs
@@ -14,14 +14,19 @@ public class Enemy : MonoBehaviour
 	public int World;
 	public int ViewRange;
 
+	private float Speed = 70.0f;
 	private int Health;
 	private bool bSawPlayer;
+	private Animator m_Animator;
+	private Rigidbody2D m_RigidBody;
 	private static Transform PlayerTransform;
-
+	
 	// Use this for initialization
 	void Start ()
 	{
 		Health =  2 * World * (int)MyType;
+		m_Animator = GetComponent<Animator>();
+		m_RigidBody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -43,6 +48,10 @@ public class Enemy : MonoBehaviour
 		if(Vector2.SqrMagnitude(PlayerTransform.position - transform.position) <= ViewRange*ViewRange)
 		{
 			//Walk towards the player
+			Vector2 direction = (PlayerTransform.position - transform.position).normalized;
+			m_RigidBody.velocity = new Vector2(direction.x * Speed, direction.y * Speed	);
+			m_Animator.SetFloat("XVelocity",m_RigidBody.velocity.x);
+			m_Animator.SetFloat("YVelocity",m_RigidBody.velocity.y);
 		}
 	}
 }
