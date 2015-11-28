9c8827ab7045fc8b9a2931e7f02737de282d4a6e
Julian Carlson <Julian_carlson@live.com>
2015-11-28 04:25:53 -0600

    stuff

--- GameJamGame/Assets/Scripts/Manager/FireBallManager.cs
+++ GameJamGame/Assets/Scripts/Manager/FireBallManager.cs
@@ -5,14 +5,14 @@ public class FireBallManager : MonoBehaviour {
 
 	public Fireball[] m_Fireballs;
 
-	public void ShootFireball(int initalDir, Enemy target = null)
+	public void ShootFireball(int initalDir,float damage , Enemy target = null)
 	{
 		for(int i = 0; i < m_Fireballs.Length; ++i)
 		{
 			if(!m_Fireballs[i].gameObject.activeSelf)
 			{
 				//m_Fireballs[i].gameObject.SetActive(true);
-				m_Fireballs[i].Shoot(initalDir,target);
+				m_Fireballs[i].Shoot(initalDir, damage,target);
 				break;
 			}
 		}
