80616a2750f486572eaf44d3d3b62908b380888e
macasillas <miguel@miguelcasillas.com>
2015-11-28 08:40:41 -0600

    Nova skill

--- GameJamGame/Assets/Scripts/Manager/SkillsManager.cs
+++ GameJamGame/Assets/Scripts/Manager/SkillsManager.cs
@@ -14,13 +14,29 @@ public class SkillsManager : MonoBehaviour
 	public static bool bHasSkill2 = false;
 	public static bool bHasSkill3 = false;
 
+	public GameObject NovaProjectilePrefab;
+
 	private float SkillCooldown1 = 0.0f;
 	private float SkillCooldown2 = 0.0f;
 	private float SkillCooldown3 = 0.0f;
 
+	private NovaProjectile [] NovaProjectilesPool;
+
 	void OnEnable()
 	{
-		CheckSkills();
+		//CheckSkills();
+	}
+
+	void Start()
+	{
+		NovaProjectilesPool = new NovaProjectile[8];
+		for(int i = 0; i < NovaProjectilesPool.Length; i++)
+		{
+			GameObject temp = Instantiate(NovaProjectilePrefab);
+			NovaProjectilesPool[i] = temp.GetComponent<NovaProjectile>();
+			temp.SetActive(false);
+		}
+
 	}
 
 	public void CheckSkills()
@@ -49,7 +65,7 @@ public class SkillsManager : MonoBehaviour
 		SkillCooldown2 = Mathf.Max(0.0f, SkillCooldown2 - Time.deltaTime);
 		SkillCooldown3 = Mathf.Max(0.0f, SkillCooldown3 - Time.deltaTime);
 
-		if(SkillCooldown1 <= 0.0f)
+		/*if(SkillCooldown1 <= 0.0f)
 		{
 			Skill1.interactable = true;
 		}
@@ -60,12 +76,28 @@ public class SkillsManager : MonoBehaviour
 		if(SkillCooldown3 <= 0.0f)
 		{
 			Skill3.interactable = true;
-		}
+		}*/
 
 		if(bHasSkill1 && Input.GetKeyDown(KeyCode.Alpha1) && SkillCooldown1 <= 0.0f)
 		{
-			Skill1.interactable = false;
-			//Fire nova
+			//Skill1.interactable = false;
+			//Dark fire nova
+			NovaProjectilesPool[0].SetDir(new Vector2(-1.0f, 0.0f));
+			NovaProjectilesPool[1].SetDir(new Vector2(1.0f, 0.0f));
+			NovaProjectilesPool[2].SetDir(new Vector2(0.0f, 1.0f));
+			NovaProjectilesPool[3].SetDir(new Vector2(0.0f, -1.0f));
+			NovaProjectilesPool[4].SetDir(new Vector2(-0.6f, 0.6f));
+			NovaProjectilesPool[5].SetDir(new Vector2(0.6f, 0.6f));
+			NovaProjectilesPool[6].SetDir(new Vector2(0.6f, -0.6f));
+			NovaProjectilesPool[7].SetDir(new Vector2(-0.6f, -0.6f));
+
+			for(int i = 0; i < NovaProjectilesPool.Length; i++)
+			{
+				NovaProjectilesPool[i].gameObject.SetActive(true);
+				NovaProjectilesPool[i].transform.position = transform.position + new Vector3(NovaProjectilesPool[i].Direction.x,
+				                                                                             NovaProjectilesPool[i].Direction.y,
+				                                                                             0.0f) * 0.3f;
+			}
 		}
 
 		if(bHasSkill2 && Input.GetKeyDown(KeyCode.Alpha2) && SkillCooldown2 <= 0.0f)
--- GameJamGame/Assets/Scripts/NovaProjectile.cs
+++ GameJamGame/Assets/Scripts/NovaProjectile.cs
@@ -3,18 +3,44 @@ using System.Collections;
 
 public class NovaProjectile : MonoBehaviour
 {
+	private float DeactivationTimer = 0.0f;
 	public Vector2 Direction;
+	private float Speed = 100.0f;
+	private float Damage = 5.0f;
 
-	// Use this for initialization
-	void Start ()
+	public void SetDir(Vector2 _dir)
 	{
+		Direction = _dir;
+		Direction.Normalize();
+	}
+
+	void OnEnable()
+	{
+		DeactivationTimer = 0.0f;
+		GetComponent<Rigidbody2D>().velocity = new Vector2(Direction.x * Speed * Time.deltaTime, Direction.y * Speed * Time.deltaTime);
+		float angle = Mathf.Atan2(Direction.y, Direction.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+	}
 	
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		Enemy pm = col.gameObject.GetComponent<Enemy>();
+		if(pm)
+		{
+			pm.TakeDamage(Damage);
+			gameObject.SetActive(false);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	
+		Debug.Log("VELOCITY: " + GetComponent<Rigidbody2D>().velocity);
+		DeactivationTimer += Time.deltaTime;
+		if(DeactivationTimer >= 3.0f)
+		{
+			gameObject.SetActive(false);
+		}
 	}
 }
 
